7f0adfd9538c8710727deb5349b37460494af17e
alvizcarlos <alvixkarlozz@hotmail.com>
2024-02-16 16:23:00 -0500

    Se mejoro y pulio el codigo, agregando y quitando algunas cosas

--- js/script.js
+++ js/script.js
@@ -1,29 +1,11 @@
 function reservarVuelo(destino) {
-    var listaReservas = document.getElementById('listaReservas');
-    var nuevaReserva = document.createElement('li');
-    nuevaReserva.textContent = 'Reservado vuelo a ' + destino;
-    listaReservas.appendChild(nuevaReserva);
+    
 }
 
-document.getElementById('formularioBusqueda').addEventListener('submit', function(event) {
-    event.preventDefault();
-});
-
-
-        // recordatorios de viaje
-        function agregarRecordatorioViaje(evento, fecha) {
-            var listaRecordatorios = document.getElementById('recordatoriosViaje');
-            var nuevoRecordatorio = document.createElement('li');
-            nuevoRecordatorio.textContent = evento + ' - ' + fecha;
-            listaRecordatorios.appendChild(nuevoRecordatorio);
-        }
-
-                // comentarios
-
 function agregarComentario(destino) {
-    var nuevoComentario = document.getElementById('nuevoComentario' + destino).value;
-    var listaComentarios = document.getElementById('comentarios' + destino);
-    var nuevoItemComentario = document.createElement('li');
-    nuevoItemComentario.textContent = nuevoComentario;
-    listaComentarios.appendChild(nuevoItemComentario);
-        }
+    var listaComentarios = document.getElementById('listaComentarios' + destino);
+    var nuevoComentario = document.createElement('li');
+    var nuevoComentarioTexto = document.getElementById('nuevoComentario' + destino).value;
+    nuevoComentario.textContent = nuevoComentarioTexto;
+    listaComentarios.appendChild(nuevoComentario);
+}
